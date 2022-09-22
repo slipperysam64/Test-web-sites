00026bf829cb7e76fddd0004fbb9d1ec473363c1
Brandon <brandon@slantedshots.com>
2022-09-21 22:22:18 -0500

    new additions 1)added html and css for Pies and Contact 2)added 2 new pie images 3)added js file for pies site

---
+++ pieSite/js/pies.js
@@ -0,0 +1,8 @@
+
+const orderButtons = document.querySelectorAll("button[data-order]");
+
+orderButtons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+        const button = e.currentTarget
+    })
+})
